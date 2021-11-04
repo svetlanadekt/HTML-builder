73de0cdade243df5e3af6a515069b9f32487d55a
Svetlana <sveta-dekt@tut.by>
2021-11-04 16:35:55 +0300

    feat: 4 tasks

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,54 @@
+const fs = require('fs');
+const path = require('path');
+const folderCopy = path.join(__dirname,'copy-files');
+const folder = path.join(__dirname,'files');
+
+
+fs.stat(folderCopy, function(err) {
+  if (err) { 
+    fs.mkdir(folderCopy, {recursive: true}, (err) => {
+      if (err) throw err;
+    });
+    
+    
+    fs.readdir(folder, (err, data) => {  // получили массив из файлов
+      data.forEach(file => {
+        fs.copyFile(path.join(folder, file), path.join(folderCopy, file), (err) => {
+          if (err) throw err;
+        }); 
+      });
+    });
+    
+  } else {
+    fs.readdir(folderCopy, (err, files) => {
+      if (err) throw err;
+    
+      for (const file of files) {
+        fs.unlink(path.join(folderCopy, file), (err) => {
+          if (err) throw err;
+        });
+      }
+    });
+
+    fs.mkdir(folderCopy, {recursive: true}, (err) => {
+      if (err) throw err;
+    });
+    
+    fs.readdir(folder, (err, data) => {  // получили массив из файлов
+      data.forEach(file => {
+        fs.copyFile(path.join(folder, file), path.join(folderCopy, file), (err) => {
+          if (err) throw err;
+        }); 
+      });
+    });  
+  }
+});
+
+
+
+
+
+
+
+
 
